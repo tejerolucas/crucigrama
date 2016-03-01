c7fb4f932f9bc351703c9dc567fe2c0a85561aff
Condurzo <maximiliano.condurzo@tictapps.com>
2016-03-01 17:21:12 -0300

    Sección Cartas. Sin Grafica y Grafica Demo.

---
+++ Assets/Scripts/Home/HomeSelector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeSelector : MonoBehaviour {
+
+	public static string NombreCarta;
+	public GameObject SeccionCrucigramas;
+	public GameObject SeccionCartas;
+
+	void Update() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 100)) {
+			NombreCarta = hit.collider.name.ToString();
+		}
+	}
+
+	void SelectCrucigramas () {
+		SeccionCrucigramas.SetActive(true);
+		SeccionCartas.SetActive(false);
+	}
+	
+	void SelectCartas () {
+		SeccionCrucigramas.SetActive(false);
+		SeccionCartas.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/Home/SeleccionCartas.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeleccionCartas : MonoBehaviour {
+
+	public GameObject[] cartasLista;
+	public GameObject[] cartasGrandes;
+	public GameObject FondoGris;
+	public string CartaGrande = "Grande";
+	public string CartaTmp;
+
+
+	void CartaChicaNoMostrar () {
+		foreach (GameObject obj in cartasLista){
+			obj.active = false;
+		}
+		AbrirCartaGrande();
+		FondoGris.SetActive(true);
+	}
+
+	void CartaChicaMostrar () {
+		foreach (GameObject obj in cartasLista){
+			obj.active = true;
+		}
+		CerrarCartaGrande();
+		FondoGris.SetActive(false);
+	}
+
+	void AbrirCartaGrande(){
+		foreach (GameObject obj in cartasGrandes){
+			CartaTmp = HomeSelector.NombreCarta + CartaGrande;
+			if(obj.name == CartaTmp){
+				obj.active = true;
+			}
+		}
+	}
+
+	void CerrarCartaGrande(){
+		foreach (GameObject obj in cartasGrandes){
+			obj.active = false;
+			}
+		}
+
+
+}
